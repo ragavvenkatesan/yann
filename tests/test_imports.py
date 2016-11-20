dc2b90f92e7dd70fc345f58afc428c9cb843e046
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2016-11-19 18:16:08 -0700

    Assert Error fixed

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,19 +5,19 @@ written unit tests before, so this is my template for them.
 import imp 
 
 def test_progressbar():
-    assertTrue(imp.find_module('progressbar'))
+    assert imp.find_module('progressbar')
 
 def test_skdata():
-    assertTrue(imp.find_module('skdata'))
+    assert imp.find_module('skdata')
 
 def test_scipy():
-    assertTrue(imp.find_module('scipy'))
+    assert imp.find_module('scipy')
 
 def test_numpy():    
-    assertTrue(imp.find_module('numpy'))
+    assert imp.find_module('numpy')
 
 def test_theano():
-    assertTrue(imp.find_module('theano'))
+    assert imp.find_module('theano')
 
 def test_yann():
-    assertTrue(imp.find_module('yann'))
+    assert imp.find_module('yann')
