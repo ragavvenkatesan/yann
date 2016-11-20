bug fix with test function for temperature softmax.

tests/test_activations.py
@@ -7,15 +7,14 @@ class TestActivations:
     @classmethod
     def setup_class(self):
         self.theano_input = T.matrix()
-        self.numpy_input = np.random.uniform(-4, 4, (5, 5))  # Create some 5X5 matrix randomly     
+        self.numpy_input = np.random.uniform(-4, 4, (5, 1))  # Create some 5X5 matrix randomly     
     def Abs(self, x): return np.abs(x)
     def ReLU(self, x): return x * (x > 0)
     def Sigmoid(self, x): return 1 / (1 + np.exp(-x))
     def Tanh(self, x): return np.tanh(x)
     def Softmax(self, x): return (np.exp(x).T / np.exp(x).sum(-1)).T
     def Squared(self, x): return x**2
-    def TemperatureSoftmax(self, x, t): return (np.exp(float(x)/t).T / np.exp(float(x)/t).sum(-1)).T
-           
+    def TemperatureSoftmax(self, x, t): return (np.exp(float(x)/t).T / np.exp(float(x)/t).sum(-1)).T           
 
     def test_abs(self):         
         theano_result = A.Abs(self.theano_input).eval({self.theano_input: self.numpy_input})