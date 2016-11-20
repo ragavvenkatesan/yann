9892f78c15412816f8da877887583ab79174bb16
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2016-11-19 23:50:54 -0700

    Let's see if this works.

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -5,11 +5,8 @@ import yann.core.activations as A
 
 @pytest.fixture(scope='module')
 def generator (activation):
-    def test(self):
-        theano_input = T.matrix()
-        numpy_input = np.random.uniform(-4, 4, (5, 5))  # Create some 5X5 matrix randomly     
+    def test(self):    
         theano_test_function = getattr(A,activation)
-        np_test_function = getattr(self, activation) # for every activation write a np version 
         theano_result = theano_test_function(theano_input).eval({theano_input: numpy_input})    
         np_result = np_test_function(numpy_input)            
         assert np.allclose(theano_result, np_result)       
@@ -17,16 +14,23 @@ def generator (activation):
 
 class TestActivations:
     @classmethod
-    def setup_class(cls):
-        test_activations = ['Abs','ReLU','Sigmoid','Tanh','Softmax','Squared']            
+    def setup_class(self):
+        test_activations = ['Abs','ReLU','Sigmoid','Tanh','Softmax','Squared']
+        self.theano_input = T.matrix()
+        self.numpy_input = np.random.uniform(-4, 4, (5, 5))  # Create some 5X5 matrix randomly 
         for activation in test_activations:
             test_name = 'test_' + activation
             test = generator(activation)
-            setattr(self,test_name, test)    
+            setattr(self,test_name, test) 
+        
     def Abs(self, x): return np.abs(x)
     def ReLU(self, x): return x * (x > 0)
     def Sigmoid(self, x): return 1 / (1 + np.exp(-x))
     def Tanh(self, x): return np.tanh(x)
     def Softmax(self, x): return (np.exp(x).T / np.exp(x).sum(-1)).T
-    def Squared(self, x): return x**2        
+    def Squared(self, x): return x**2       
+    def test_abs(self):         
+        theano_result = A.Abs(self.theano_input).eval({self.theano_input: self.numpy_input})
+        np_result = Abs(self.numpy_input)
+        assert np.allclose(theano_result, np_result)
 
