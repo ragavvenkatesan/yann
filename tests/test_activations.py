bug fix with temperature softmax.

tests/test_activations.py
@@ -7,9 +7,7 @@ class TestActivations:
     @classmethod
     def setup_class(self):
         self.theano_input = T.matrix()
-        self.numpy_input = np.random.uniform(-4, 4, (5,5))  # Create some 5X5 matrix randomly
-        self.softmax_input = np.random.uniform(-4, 4, (5,5))  # Create some 5X5 matrix randomly
-        
+        self.numpy_input = np.random.uniform(-4, 4, (5,5))  # Create some 5X5 matrix randomly        
 
     def Abs(self, x): return np.abs(x)
     def ReLU(self, x): return x * (x > 0)
@@ -20,7 +18,7 @@ class TestActivations:
             expo = np.exp(x / float(temp))
             return expo / expo.sum(axis=1)
         else:
-            return (np.exp(x).T / np.exp(x).sum(-1)).T   
+            return np.exp(x) / np.exp(x).sum(axis = 1))  
     def Squared(self, x): return x**2
     def TemperatureSoftmax(self, x, t): return (np.exp(float(x)/t).T / np.exp(float(x)/t).sum(-1)).T           
 
@@ -52,8 +50,8 @@ class TestActivations:
     def test_temperature_softmax(self):   
         t = np.random.uniform(2, 10, 1)     
         theano_result = A.Softmax(self.theano_input, 
-                                          temp = t ).eval({self.theano_input: self.softmax_input})
-        np_result = self.Softmax(self.softmax_input, t = t)
+                                          temp = t ).eval({self.theano_input: self.numpy_input})
+        np_result = self.Softmax(self.numpy_input, t = t)
         assert np.allclose(theano_result, np_result)                                     
 
     def test_squared(self):         

yann/core/activations.py
@@ -79,7 +79,7 @@ def Softmax(x, temp = 1):
     """
     if temp != 1:
         expo = T.exp(x / float(temp))
-        return expo / T.addbroadcast(expo.sum(axis=1),1)
+        return expo / expo.sum(axis=-1)
     else:
         return T.nnet.softmax(x)               
    