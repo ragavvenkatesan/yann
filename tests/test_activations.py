2d2f46afe6c5cfb0546ad3e0a3dd0cb394714772
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2016-11-20 00:06:15 -0700

    All activations are done.

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -14,9 +14,33 @@ class TestActivations:
     def Tanh(self, x): return np.tanh(x)
     def Softmax(self, x): return (np.exp(x).T / np.exp(x).sum(-1)).T
     def Squared(self, x): return x**2       
-    
+
     def test_abs(self):         
         theano_result = A.Abs(self.theano_input).eval({self.theano_input: self.numpy_input})
         np_result = self.Abs(self.numpy_input)
         assert np.allclose(theano_result, np_result)
 
+    def test_relu(self):         
+        theano_result = A.ReLU(self.theano_input).eval({self.theano_input: self.numpy_input})
+        np_result = self.ReLU(self.numpy_input)
+        assert np.allclose(theano_result, np_result)      
+
+    def test_sigmoid(self):         
+        theano_result = A.Sigmoid(self.theano_input).eval({self.theano_input: self.numpy_input})
+        np_result = self.Sigmoid(self.numpy_input)
+        assert np.allclose(theano_result, np_result)        
+
+    def test_tanh(self):         
+        theano_result = A.Tanh(self.theano_input).eval({self.theano_input: self.numpy_input})
+        np_result = self.Tanh(self.numpy_input)
+        assert np.allclose(theano_result, np_result)    
+
+    def test_softmax(self):         
+        theano_result = A.Softmax(self.theano_input).eval({self.theano_input: self.numpy_input})
+        np_result = self.Softmax(self.numpy_input)
+        assert np.allclose(theano_result, np_result)                            
+
+    def test_squared(self):         
+        theano_result = A.Squared(self.theano_input).eval({self.theano_input: self.numpy_input})
+        np_result = self.Squared(self.numpy_input)
+        assert np.allclose(theano_result, np_result)     
