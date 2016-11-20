085a97f116506ad464347103c72589b57e028673
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2016-11-19 18:13:26 -0700

    migrate from unit test to purest

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,30 +3,21 @@ Just a dummy test I'm writing to get used to the idea of unittesting. I've never
 written unit tests before, so this is my template for them.
 """
 import imp 
-import unittest
 
-class TestImports(unittest.TestCase):
-    
-    def test_progressbar(self):
-        self.assertTrue(imp.find_module('progressbar'))
+def test_progressbar():
+    assertTrue(imp.find_module('progressbar'))
 
-    def test_skdata(self):
-        self.assertTrue(imp.find_module('skdata'))
+def test_skdata():
+    assertTrue(imp.find_module('skdata'))
 
-    def test_scipy(self):
-        self.assertTrue(imp.find_module('scipy'))
+def test_scipy():
+    assertTrue(imp.find_module('scipy'))
 
-    def test_numpy(self):    
-        self.assertTrue(imp.find_module('numpy'))
+def test_numpy():    
+    assertTrue(imp.find_module('numpy'))
 
-    def test_theano(self):
-        self.assertTrue(imp.find_module('theano'))
+def test_theano():
+    assertTrue(imp.find_module('theano'))
 
-    def test_yann(self):
-        self.assertTrue(imp.find_module('yann'))
-
-"""
-if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestImports)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-"""
+def test_yann():
+    assertTrue(imp.find_module('yann'))
