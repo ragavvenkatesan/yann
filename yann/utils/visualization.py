1f6e25e35d95e13f4c04fbb1a7efe888bc1516e9
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2016-11-21 02:20:16 -0700

    pretty networks printing of network.

--- yann/utils/visualization.py
+++ yann/utils/visualization.py
@@ -12,11 +12,25 @@ def draw_network(graph, filename = 'network.pdf', show = False):
     """
     pos=nx.spectral_layout(graph)
     labels = {}
-    node_size = {}
+    node_size = []
+    pos = nx.graphviz_layout(g, prog="dot")
+    node_list = []
     for node in graph.nodes(): 
-        labels[node] = node    
-        node_size[node] = len(node) * 10       
+        labels [ node ] = node    
+        node_size.append ( len(node) * 1000 )
+        node_list.append ( node )       
 
+    nx.draw_networkx_nodes( G = graph, 
+                            pos = pos,
+                            node_list = node_list,
+                            node_size = node_size,
+                            node_color = 'g',
+                            node_shape = 'o' )
+    nx.draw_networkx_edges(G = graph, pos = pos)
+    nx.draw_networkx_labels(G = graph, pos = pos , labels = labels)
+    plt.savefig(filename)
+
+    """"
     nx.draw(    graph,
                 pos = pos,
                 node_size = 1000,
@@ -24,7 +38,7 @@ def draw_network(graph, filename = 'network.pdf', show = False):
                 node_shape = 'o',
                 width = 1.0,
                 labels = labels )    
-
+    """
     plt.savefig(filename)
     if show == True:
         plt.show()
