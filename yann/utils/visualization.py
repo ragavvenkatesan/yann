2f63a23e72a0e24e10b97e544819a0bde983ed18
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2016-11-21 01:58:50 -0700

    trying this new thing called nope

--- yann/utils/visualization.py
+++ yann/utils/visualization.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import networkx as nx
+from nxpd import draw 
 
 def draw_network(graph, filename = 'network.pdf', show = False):
     """
@@ -14,13 +14,13 @@ def draw_network(graph, filename = 'network.pdf', show = False):
     labels = {}
     for node in graph.nodes(): 
         labels[node] = node
-    nx.draw(    graph,
-                pos = pos,
-                node_size = 1000,
-                node_color = 'g',
-                node_shape = 'o',
-                width = 1.0,
-                labels = labels )
+    draw(   graph,
+            pos = pos,
+            node_size = 1000,
+            node_color = 'g',
+            node_shape = 'o',
+            width = 1.0,
+            labels = labels )
     plt.savefig(filename)
     if show == True:
         plt.show()
