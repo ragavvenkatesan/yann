b67866c5c253da3bf566b6967043f2e15810c1ea
Anchit Agarwal <anchit008@gmail.com>
2017-01-27 02:49:58 -0700

    transform.py edit

--- yann/layers/transform.py
+++ yann/layers/transform.py
@@ -41,6 +41,8 @@ class rotate_layer (layer):
                                                 dtype = theano.config.floatX )
 
             theta = numpy.zeros((input_shape[0],2,3),dtype='float32')
+            import pdb
+            pdb.set_trace()
             theta[:,0,0] = numpy.cos(angle[:,0]*180)
             theta[:,0,1] = -numpy.sin(angle[:,0]*180)
             theta[:,1,0] = numpy.sin(angle[:,0]*180)
