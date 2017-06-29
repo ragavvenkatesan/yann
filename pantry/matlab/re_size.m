e8169a0a8bd178c5f279d3a6db99666f3dcc63d6
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2017-06-29 15:20:41 -0700

    svhn 28 28

--- pantry/matlab/re_size.m
+++ pantry/matlab/re_size.m
@@ -1,10 +1,10 @@
-function out = re_size (imgs, size)
+function out = re_size (imgs, shape)
     
-    imgs = reshape(imgs, [size(imgs,1),sqrt(size(imgs,2)),sqrt(size(imgs,2)),3]);
-    out = zeros(size);
+    imgs = reshape(imgs, [size(imgs,1),sqrt(size(imgs,2)/3),sqrt(size(imgs,2)/3),3]);
+    out = zeros([size(imgs,1),shape]);
     
     for i = 1:size(imgs,1)
-       out(i,:,:) = rgb2gray(squeeze(imgs(i,:,:,:)));
+       out(i,:,:) = imresize(rgb2gray(uint8(squeeze(imgs(i,:,:,:)))),[28,28]);
     end
 end
     
