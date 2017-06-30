b2b9a86880baaea600c3c4b7a97901751e4ee45e
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2017-06-30 16:06:26 -0700

    adding a split continual

--- yann/special/datasets.py
+++ yann/special/datasets.py
@@ -1590,6 +1590,413 @@ class split_only_train(setup_dataset):
         else:
             return (data_x, data_y)
 
+class split_continual(setup_dataset):
+    
+    """
+    Inheriting from the setup dataset. This method will produce datasets setup for continual 
+    learning systems. 
+    """    
+    def __init__(self,
+                 dataset_init_args,
+                 save_directory = '_datasets',
+                 verbose = 0,
+                 n_classes = 10,
+                 **kwargs): 
+        """
+        This is just a re-use of the setup_dataset. This will construct the split dataset. 
+        In the default arguments listed below, this will create data 2,3 classes for training, 
+        will test on the classes of 0,1,2,3 and not have classes 4-9 in the dataset.
+
+        Args:
+            split_args: Is a dictionary of the form,
+                splits = { 
+                        "train"             : [2,3],
+                        "test"              : [0,1,2,3],
+                    }   
+            n_classes: supply the number of classes in the dataset. 
+
+        Notes:
+            Arguments are the same as in the case of setup_dataset. With the addtion of one extra
+            argument  - split_args.
+        """
+        if "preprocess_init_args" in kwargs.keys():
+            self.preprocessor = kwargs['preprocess_init_args']
+        else:
+            self.preprocessor =  {
+                            "normalize"     : True,
+                            "ZCA"           : False,
+                            "grayscale"     : False,
+                            "zero_mean"		: True,
+                            }        
+
+        if "split_args" in kwargs.keys():
+            self.splits = kwargs['split_args']
+        else:
+            self.splits =  { 
+                        "train"             : [2,3],
+                        "test"              : [0,1,2,3],
+                    }  
+
+        self.n_classes = len(self.splits["test"])
+        
+        super(split_continual,self).__init__( 
+                                        dataset_init_args = dataset_init_args,
+                                        save_directory = save_directory,
+                                        preprocess_init_args = self.preprocessor,
+                                        verbose = 1)
+
+    def _create_skdata_mnist(self, verbose = 1):
+        """
+        Interal function. Use this to create mnist and cifar image datasets
+        This is modfied for the split dataset from the original ``setup_dataset`` class.
+        """
+        if verbose >=2:
+            print (".. Creating a split dataset")
+        if verbose >=3:
+            print("... Importing " + self.name + " from skdata")
+        data = getattr(thismodule, 'load_skdata_' + self.name)()
+
+        if verbose >=2:
+            print(".. setting up dataset")
+            print(".. training data")
+
+        data_x, data_y, data_y1 = self._split_data (data = data[0], keep = self.split["train"])
+
+        data_x = preprocessing ( data = data_x,
+                                 height = self.height,
+                                 width = self.width,
+                                 channels = self.channels,
+                                 args = self.preprocessor )
+
+        training_sample_size = data_x.shape[0]
+        training_batches_available  = int(numpy.floor(training_sample_size / self.mini_batch_size))
+
+        if training_batches_available < self.batches2train * self.mini_batches_per_batch[0]:
+            self.mini_batches_per_batch = ( training_batches_available/self.batches2train,
+                                            self.mini_batches_per_batch [1],
+                                            self.mini_batches_per_batch [2] )
+
+        if self.batches2train * self.mini_batches_per_batch[0] < self.cache_images[0]:
+            self.cache_images = (self.mini_batches_per_batch[0] * self.mini_batch_size, \
+                                        self.cache_images[1],  self.cache_images[2])
+
+        data_x = data_x[:self.cache_images[0]]
+        data_y = data_y[:self.cache_images[0]]                
+
+        loc = self.root + "/train/"
+        data_x = check_type(data_x, theano.config.floatX)
+        data_y = check_type(data_y, theano.config.floatX)
+
+        for batch in xrange(self.batches2train):
+            start_index = batch * self.cache_images[0]
+            end_index = start_index + self.cache_images[0]
+            data2save = (data_x [start_index:end_index,], data_y[start_index:end_index,] )
+            pickle_dataset(loc = loc, data = data2save, batch=batch)
+
+        if verbose >=2:
+            print(".. validation data ")
+
+        data_x, data_y, data_y1 = self._split_data (data = data[1], keep = self.splits["valid"])
+        data_x = preprocessing ( data = data_x,
+                                 height = self.height,
+                                 width = self.width,
+                                 channels = self.channels,
+                                 args = self.preprocessor )
+
+        validation_sample_size = data_x.shape[0]
+        validation_batches_available = int(numpy.floor(
+                                                validation_sample_size / self.mini_batch_size))
+
+        if validation_batches_available < self.batches2validate * self.mini_batches_per_batch[1]:
+            self.mini_batches_per_batch = ( self.mini_batches_per_batch [0],
+                                            validation_batches_available/self.batches2validate,
+                                            self.mini_batches_per_batch [2] )
+
+        if self.batches2validate * self.mini_batches_per_batch[1] < self.cache_images[1]:
+            self.cache_images = (   self.cache_images[0],\
+                                    self.mini_batches_per_batch[1] * self.mini_batch_size, \
+                                    self.cache_images[2])
+
+        data_x = data_x[:self.cache_images[1]]
+        data_y = data_y[:self.cache_images[1]]
+
+        loc = self.root + "/valid/"
+        data_x = check_type(data_x, theano.config.floatX)
+        data_y = check_type(data_y, theano.config.floatX)
+
+        for batch in xrange(self.batches2validate):
+            start_index = batch * self.cache_images[1]
+            end_index = start_index + self.cache_images[1]
+            data2save = (data_x [start_index:end_index,], data_y[start_index:end_index,] )
+            pickle_dataset(loc = loc, data = data2save, batch=batch)
+
+        if verbose >=2:
+            print(".. testing data ")
+
+        data_x, data_y, data_y1 = self._split_data (data = data[2], keep = self.splits["test"])
+        data_x = preprocessing ( data = data_x,
+                                 height = self.height,
+                                 width = self.width,
+                                 channels = self.channels,
+                                 args = self.preprocessor )
+        testing_sample_size = data_x.shape[0]
+        testing_batches_available = int(numpy.floor(testing_sample_size / self.mini_batch_size))
+
+        if testing_batches_available < self.batches2test * self.mini_batches_per_batch[2]:
+            self.mini_batches_per_batch = ( self.mini_batches_per_batch [0],
+                                            self.mini_batches_per_batch [1],
+                                            testing_batches_available/self.batches2test )
+
+        if self.batches2test * self.mini_batches_per_batch[2] < self.cache_images[2]:
+            self.cache_images = (   self.cache_images[0],\
+                                    self.cache_images[1], \
+                                    self.mini_batches_per_batch[2] * self.mini_batch_size )
+
+        data_x = data_x[:self.cache_images[2]]
+        data_y = data_y[:self.cache_images[2]]
+
+        loc = self.root + "/test/"
+        data_x = check_type(data_x, theano.config.floatX)
+        data_y = check_type(data_y, theano.config.floatX)
+
+        for batch in xrange(self.batches2test):
+            start_index = batch * self.cache_images[2]
+            end_index = start_index + self.cache_images[2]
+            data2save = (data_x [start_index:end_index,], data_y[start_index:end_index,] )
+            pickle_dataset(loc = loc, data = data2save, batch=batch)
+
+        dataset_args = {
+                "location"                  : self.root,
+                "mini_batch_size"           : self.mini_batch_size,
+                "cache_batches"             : self.mini_batches_per_batch,
+                "batches2train"             : self.batches2train,
+                "batches2test"              : self.batches2test,
+                "batches2validate"          : self.batches2validate,
+                "height"                    : self.height,
+                "width"                     : self.width,
+                "channels"              : 1 if self.preprocessor ["grayscale"] else self.channels,
+                "cache"                     : self.cache,
+                "splits"                    : self.splits
+                }
+
+        assert ( self.height * self.width * self.channels == numpy.prod(data_x.shape[1:]) )
+        f = open(self.root +  '/data_params.pkl', 'wb')
+        cPickle.dump(dataset_args, f, protocol=2)
+        f.close()        
+
+    def _mat2yann (self, verbose = 1):
+        """
+        Interal function. Use this to create matlab image datasets
+        This is modfied for the split dataset from the original ``setup_dataset`` class.
+        """
+        if verbose >=2:
+            print (".. Creating a split dataset")
+
+        type = 'train'
+        if verbose >= 2:
+            print ( ".. creating data " + type )
+        
+        batches = self.batches2train
+        new = True
+        for batch in xrange(batches):       # for each batch_i file....
+            if verbose >= 3:
+                print ( "... batch " +str(batch) )
+
+            data_x_batch, data_y_batch = load_data_mat(location = self.location, 
+                                                batch = batch, 
+                                                type_set = type,
+                                                height = self.height,
+                                                width = self.width,
+                                                channels = self.channels)
+            if new is True:
+                data_x = data_x_batch
+                data_y = data_y_batch
+                new = False
+            else:
+                data_x = numpy.concatenate( (data_x, data_x_batch) , axis = 0)
+                data_y = numpy.concatenate( (data_y, data_y_batch) , axis = 0)
+
+        data_x, data_y  = self._split_data (data = ( data_x, data_y ), y1 = False, 
+                                                                    keep = self.splits["train"] )
+        data_x = preprocessing ( data = data_x,
+                                height = self.height,
+                                width = self.width,
+                                channels = self.channels,
+                                args = self.preprocessor )
+
+        training_sample_size = data_x.shape[0]
+        training_mini_batches_available  = int(numpy.floor(training_sample_size / self.mini_batch_size))
+
+        if training_mini_batches_available < self.batches2train * self.mini_batches_per_batch[0]:
+            #self.mini_batches_per_batch = ( training_batches_available/self.batches2train,
+            #                                self.mini_batches_per_batch [1],
+            #                                self.mini_batches_per_batch [2] )
+            self.batches2train = int(numpy.floor(training_mini_batches_available / self.mini_batches_per_batch[0]))         
+
+        loc = self.root + "/train/"
+        data_x = check_type(data_x, theano.config.floatX)
+        data_y = check_type(data_y, theano.config.floatX)
+
+        for batch in xrange(self.batches2train):
+            start_index = batch * self.cache_images[0]
+            end_index = start_index + self.cache_images[0]
+            data2save = (data_x [start_index:end_index,], data_y[start_index:end_index,] )
+            pickle_dataset(loc = loc, data = data2save, batch=batch)
+
+            
+        type = 'valid'
+        if verbose >= 2:
+            print ( ".. creating data " + type )
+        batches = self.batches2validate
+        new = True
+        del(data_x)
+        del(data_y)
+
+        for batch in xrange(batches):       # for each batch_i file....
+            if verbose >= 3:
+                print ( "... batch " +str(batch) )
+
+            data_x_batch, data_y_batch = load_data_mat(location = self.location, 
+                                                batch = batch, 
+                                                type_set = type,
+                                                height = self.height,
+                                                width = self.width,
+                                                channels = self.channels)
+
+            if new is True:
+                data_x = data_x_batch
+                data_y = data_y_batch
+                new = False
+            else:
+                data_x = numpy.concatenate( (data_x, data_x_batch) , axis = 0)
+                data_y = numpy.concatenate( (data_y, data_y_batch) , axis = 0)
+
+        data_x, data_y  = self._split_data (( data_x, data_y ), y1 = False 
+                                                                , keep = self.splits["valid"] )
+        data_x = preprocessing ( data = data_x,
+                                height = self.height,
+                                width = self.width,
+                                channels = self.channels,
+                                args = self.preprocessor )
+
+
+        validation_sample_size = data_x.shape[0]
+        validation_mini_batches_available = int(numpy.floor(
+                                                validation_sample_size / self.mini_batch_size))
+
+        if validation_mini_batches_available < self.batches2validate * self.mini_batches_per_batch[1]:
+            self.batches2validate = int(numpy.floor(validation_mini_batches_available \
+                                    / self.mini_batches_per_batch[1]))
+
+        loc = self.root + "/valid/"
+        data_x = check_type(data_x, theano.config.floatX)
+        data_y = check_type(data_y, theano.config.floatX)
+
+        for batch in xrange(self.batches2validate):
+            start_index = batch * self.cache_images[1]
+            end_index = start_index + self.cache_images[1]
+            data2save = (data_x [start_index:end_index,], data_y[start_index:end_index,] )
+            pickle_dataset(loc = loc, data = data2save, batch=batch)
+
+        type = 'train'
+        if verbose >= 2:
+            print ( ".. creating data " + type )
+        batches = self.batches2test
+        new = True
+        del(data_x)
+        del(data_y)
+
+        for batch in xrange(batches):       # for each batch_i file....
+            if verbose >= 3:
+                print ( "... batch " +str(batch) )
+
+            data_x_batch, data_y_batch = load_data_mat(location = self.location, 
+                                                batch = batch, 
+                                                type_set = type,
+                                                height = self.height,
+                                                width = self.width,
+                                                channels = self.channels)
+            if new is True:
+                data_x = data_x_batch
+                data_y = data_y_batch
+                new = False
+            else:
+                data_x = numpy.concatenate( (data_x, data_x_batch) , axis = 0)
+                data_y = numpy.concatenate( (data_y, data_y_batch) , axis = 0)
+
+        data_x, data_y  = self._split_data (( data_x, data_y ), y1 = False,
+                                                            keep = self.splits["test"] )
+        data_x = preprocessing ( data = data_x,
+                                height = self.height,
+                                width = self.width,
+                                channels = self.channels,
+                                args = self.preprocessor )
+
+        testing_sample_size = data_x.shape[0]
+        testing_mini_batches_available = int(numpy.floor(testing_sample_size / self.mini_batch_size))
+
+        if testing_mini_batches_available < self.batches2test * self.mini_batches_per_batch[2]:
+            self.batches2test = int(numpy.floor(testing_mini_batches_available \
+                                    / self.mini_batches_per_batch[2]))
+
+        loc = self.root + "/test/"
+        data_x = check_type(data_x, theano.config.floatX)
+        data_y = check_type(data_y, theano.config.floatX)
+
+        for batch in xrange(self.batches2test):
+            start_index = batch * self.cache_images[2]
+            end_index = start_index + self.cache_images[2]
+            data2save = (data_x [start_index:end_index,], data_y[start_index:end_index,] )
+            pickle_dataset(loc = loc, data = data2save, batch=batch)
+
+        dataset_args = {
+                "location"                  : self.root,
+                "mini_batch_size"           : self.mini_batch_size,
+                "cache_batches"             : self.mini_batches_per_batch,
+                "batches2train"             : self.batches2train,
+                "batches2test"              : self.batches2test,
+                "batches2validate"          : self.batches2validate,
+                "height"                    : self.height,
+                "width"                     : self.width,
+                "channels"              : 1 if self.preprocessor ["grayscale"] else self.channels,
+                "cache"                     : self.cache,
+                "splits"                    : self.splits
+                }
+
+        assert ( self.height * self.width * self.channels == numpy.prod(data_x.shape[1:]) )
+        f = open(self.root +  '/data_params.pkl', 'wb')
+        cPickle.dump(dataset_args, f, protocol=2)
+        f.close()
+
+    def _split_data (self, data, keep, y1 = True):
+        """
+        This is an internal method that will split the datasets.
+
+        Args:
+            data: train, test and valid batches in a tuple.
+            keep: which labels to keep. Everythin else is dropped. 
+        
+        Returns:
+            tuple: split data in the same format as data.
+        """
+        if y1 is True:
+            data_x, data_y, data_y1  = data
+        else:
+            data_x, data_y = data
+
+        locs = numpy.zeros(len(data_y), dtype = bool)
+        for label in xrange(self.n_classes + 1):
+            # temp = numpy.zeros(len(data_y), dtype = bool)                                                
+            # temp[data_y==label] = True
+            if label in keep:                         
+                locs[data_y == label] = True
+        data_x = data_x[locs]
+        data_y = data_y[locs]
+        if y1 is True:
+            data_y1 = data_y1[locs]
+            return (data_x, data_y, data_y1)  
+        else:
+            return (data_x, data_y)
 
 if __name__ == '__main__':
     pass
